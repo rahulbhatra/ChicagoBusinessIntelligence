a80d9f3246c84304c18a229fd0a30a725306fefa
Rahul Sharma <rahulsharma@Rahuls-Air.cable.rcn.com>
2021-10-30 22:23:03 -0500

    simple microservice using go added

--- chicago_business_intelligence_backend/src/covid/covid.go
+++ chicago_business_intelligence_backend/src/covid/covid.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -8,7 +10,20 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello world")
+		log.Println("Hello World")
+		d, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			http.Error(rw, "Oops", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("Hello %s", d)
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Good bye ma ma miya")
 	})
 
 	http.ListenAndServe(":9090", nil)
