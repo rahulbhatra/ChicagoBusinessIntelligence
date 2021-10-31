0a3380a798402c97107b2aa63126d0409a6de853
Rahul Sharma <rahulsharma@Rahuls-Air.cable.rcn.com>
2021-10-30 21:48:22 -0500

    docker files added

---
+++ chicago_business_intelligence_backend/src/covid/covid.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello world")
+	})
+
+	http.ListenAndServe(":9090", nil)
+}
