b32962a3678341125421b2a6adc9719d23f369c3
Anjali-Veer <anjali.veer@hotmail.com>
2021-11-19 13:00:34 -0600

    Update main.go

--- chicago_business_intelligence_backend/gitlab.com/bi/main.go
+++ chicago_business_intelligence_backend/gitlab.com/bi/main.go
@@ -11,6 +11,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/kelvins/geocoder"
 	_ "github.com/lib/pq"
 )
 
@@ -183,22 +184,49 @@ func main() {
 	})
 
 	http.HandleFunc("/taxi_trip", func(rw http.ResponseWriter, r *http.Request) {
+		geocoder.ApiKey = "AIzaSyCctDVzYHUX6D4mXEAqbn3WoUkkOXjg3oU"
+
+		dropSql := `drop table if exists covid_daily`
+		_, err := db.Exec(dropSql)
+		if err != nil {
+			panic(err)
+		}
+
+		createSql := `CREATE TABLE IF NOT EXISTS "taxi_trip" 
+		(
+			"id"   SERIAL , 
+			"tripId" VARCHAR(255) UNIQUE, 
+			"tripStartTime" TIMESTAMP WITH TIME ZONE, 
+			"tripEndTime" TIMESTAMP WITH TIME ZONE, 
+			"pickUpLat" DOUBLE PRECISION, 
+			"pickUpLon" DOUBLE PRECISION, 
+			"dropOffLat" DOUBLE PRECISION, 
+			"dropOffLon" DOUBLE PRECISION, 
+			"pickUpZip" VARCHAR(255), 
+			"dropOffZip" VARCHAR(255), 
+			"createdAt" TIMESTAMP WITH TIME ZONE NOT NULL, 
+			"updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL, 
+			PRIMARY KEY ("id")
+		
+			);`
+
+		_, createSqlErr := db.Exec(createSql)
+		if createSqlErr != nil {
+			panic(createSqlErr)
+		}
+
 		var url1 = "https://data.cityofchicago.org/resource/wrvz-psew.json"
-		var url2 = "https://data.cityofchicago.org/resource/m6dm-c72p.json"
 
 		res1, err := http.Get(url1)
 		checkErr(err)
-
-		res2, err := http.Get(url2)
-		checkErr(err)
-
 		body1, _ := ioutil.ReadAll(res1.Body)
-		body2, _ := ioutil.ReadAll(res2.Body)
+
+		// jsonString := `[{"trip_id":"14176ffa6e52c6716e37275173f1bf8ee681866f","trip_start_timestamp":"2021-09-30T23:45:00.000","trip_end_timestamp":"2021-09-30T23:45:00.000","trip_seconds":"391","trip_miles":"1.8019","pickup_census_tract":"17031080300","dropoff_census_tract":"17031280100","pickup_community_area":"8","dropoff_community_area":"28","fare":"7.5","tip":"0","additional_charges":"2.36","trip_total":"9.86","shared_trip_authorized":false,"trips_pooled":"1","pickup_centroid_latitude":"41.9074919303","pickup_centroid_longitude":"-87.6357600901","pickup_centroid_location":{"type":"Point","coordinates":[-87.6357600901,41.9074919303]},"dropoff_centroid_latitude":"41.8853000224","dropoff_centroid_longitude":"-87.6428084655","dropoff_centroid_location":{"type":"Point","coordinates":[-87.6428084655,41.8853000224]}}
+		// ,{"trip_id":"0577e6cfe99603844b2a60bc046bdd88cbc48ad7","trip_start_timestamp":"2021-09-30T23:45:00.000","trip_end_timestamp":"2021-09-30T23:45:00.000","trip_seconds":"636","trip_miles":"4.2579","pickup_census_tract":"17031062100","dropoff_census_tract":"17031081500","pickup_community_area":"6","dropoff_community_area":"8","fare":"10","tip":"5","additional_charges":"2.83","trip_total":"17.83","shared_trip_authorized":false,"trips_pooled":"1","pickup_centroid_latitude":"41.9426918444","pickup_centroid_longitude":"-87.6517705068","pickup_centroid_location":{"type":"Point","coordinates":[-87.6517705068,41.9426918444]},"dropoff_centroid_latitude":"41.8925077809","dropoff_centroid_longitude":"-87.6262149064","dropoff_centroid_location":{"type":"Point","coordinates":[-87.6262149064,41.8925077809]}}]`
 
 		var taxiTripsArray1 TAXI_TRIPS_DATA
-		var taxiTripsArray2 TAXI_TRIPS_DATA
+		// json.Unmarshal([]byte(jsonString), &taxiTripsArray1)
 		json.Unmarshal(body1, &taxiTripsArray1)
-		json.Unmarshal(body2, &taxiTripsArray2)
 
 		ctx := context.Background()
 		tx, err := db.BeginTx(ctx, nil)
@@ -206,43 +234,96 @@ func main() {
 
 		for i := 0; i < len(taxiTripsArray1); i++ {
 
+			fmt.Println(taxiTripsArray1[i].TRIP_ID)
+
 			tripId := taxiTripsArray1[i].TRIP_ID
 			if tripId == "" {
 				tripId = "NULL"
+				continue
 			}
 
 			tripStartTime := taxiTripsArray1[i].TRIP_START_TIME
 			if tripStartTime == "" {
 				tripStartTime = "0000-00-00 00:00:00"
+				continue
 			}
 
 			tripEndTime := taxiTripsArray1[i].TRIP_END_TIME
 			if tripEndTime == "" {
 				tripEndTime = "0000-00-00 00:00:00"
+				continue
 			}
 
 			pickupLat := taxiTripsArray1[i].PICKUP_LAT
-			if pickupLat == "" {
+			if taxiTripsArray1[i].PICKUP_LAT == "" {
 				pickupLat = "0"
+				continue
 			}
 
 			pickupLon := taxiTripsArray1[i].PICKUP_LONG
 			if pickupLon == "" {
 				pickupLon = "0"
+				continue
 			}
 
 			dropoffLat := taxiTripsArray1[i].DROPOFF_LAT
 			if dropoffLat == "" {
 				dropoffLat = "0"
+				continue
 			}
 
 			dropoffLon := taxiTripsArray1[i].DROPOFF_LONG
 			if dropoffLon == "" {
 				dropoffLon = "0"
+				continue
 			}
 
-			sql := "INSERT INTO taxi_trip (tripId, tripStartTime, tripEndTime, pickupLat, pickupLon, dropoffLat, dropoffLon, pickupZip, dropoffZip) values($1, $2, $3, $4, $5, $6, $7, $8, $9)"
-			_, err = tx.ExecContext(ctx,
+			pickUpLatFloat, _ := strconv.ParseFloat(pickupLat, 64)
+			pickupLonFloat, _ := strconv.ParseFloat(pickupLon, 64)
+			pickUpLocation := geocoder.Location{
+				Latitude:  pickUpLatFloat,
+				Longitude: pickupLonFloat,
+			}
+
+			fmt.Println(pickUpLatFloat)
+			fmt.Println(pickupLonFloat)
+			fmt.Println(pickUpLocation)
+
+			pickUpAddresses, _ := geocoder.GeocodingReverse(pickUpLocation)
+			fmt.Println(&pickUpAddresses)
+			pickUpAddress := pickUpAddresses[0]
+			pickUpZip := pickUpAddress.PostalCode
+
+			dropoffLatFloat, _ := strconv.ParseFloat(dropoffLat, 64)
+			dropoffLonFloat, _ := strconv.ParseFloat(dropoffLon, 64)
+
+			dropoffLocation := geocoder.Location{
+				Latitude:  dropoffLatFloat,
+				Longitude: dropoffLonFloat,
+			}
+			dropoffAddresses, _ := geocoder.GeocodingReverse(dropoffLocation)
+			dropoffAddress := dropoffAddresses[0]
+			dropoffZip := dropoffAddress.PostalCode
+
+			createdAt := time.Now()
+			updatedAt := time.Now()
+
+			sql := `INSERT INTO taxi_trip ("tripId", "tripStartTime", "tripEndTime", "pickUpLat", "pickUpLon", "dropOffLat", "dropOffLon", "pickUpZip", 
+			"dropOffZip", "createdAt", "updatedAt") values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`
+			// _, err = tx.ExecContext(ctx,
+			// 	sql,
+			// 	tripId,
+			// 	tripStartTime,
+			// 	tripEndTime,
+			// 	pickupLat,
+			// 	pickupLon,
+			// 	dropoffLat,
+			// 	dropoffLon,
+			// 	pickUpZip,
+			// 	dropoffZip,
+			// 	createdAt, updatedAt)
+
+			_, err = db.Exec(
 				sql,
 				tripId,
 				tripStartTime,
@@ -251,17 +332,34 @@ func main() {
 				pickupLon,
 				dropoffLat,
 				dropoffLon,
-				0,
-				0)
+				pickUpZip,
+				dropoffZip,
+				createdAt, updatedAt)
 
-			if err != nil {
-				fmt.Printf("\n ERROR = ", err)
-				fmt.Printf("\n")
-				tx.Rollback()
-				return
+			// if err != nil {
+			// 	fmt.Printf("\n ERROR = ", err)
+			// 	fmt.Printf("\n")
+			// 	tx.Rollback()
+			// 	return
+			// }
+
+			_, errCreate := db.Exec(dropSql)
+			if errCreate != nil {
+				panic(errCreate)
 			}
 		}
 
+		var url2 = "https://data.cityofchicago.org/resource/m6dm-c72p.json"
+		res2, err := http.Get(url2)
+		checkErr(err)
+
+		// jsonString2 := `[{"trip_id":"14176ffa6e52c6716e37275173f1bf8ee681866f","trip_start_timestamp":"2021-09-30T23:45:00.000","trip_end_timestamp":"2021-09-30T23:45:00.000","trip_seconds":"391","trip_miles":"1.8019","pickup_census_tract":"17031080300","dropoff_census_tract":"17031280100","pickup_community_area":"8","dropoff_community_area":"28","fare":"7.5","tip":"0","additional_charges":"2.36","trip_total":"9.86","shared_trip_authorized":false,"trips_pooled":"1","pickup_centroid_latitude":"41.9074919303","pickup_centroid_longitude":"-87.6357600901","pickup_centroid_location":{"type":"Point","coordinates":[-87.6357600901,41.9074919303]},"dropoff_centroid_latitude":"41.8853000224","dropoff_centroid_longitude":"-87.6428084655","dropoff_centroid_location":{"type":"Point","coordinates":[-87.6428084655,41.8853000224]}}
+		// ,{"trip_id":"0577e6cfe99603844b2a60bc046bdd88cbc48ad7","trip_start_timestamp":"2021-09-30T23:45:00.000","trip_end_timestamp":"2021-09-30T23:45:00.000","trip_seconds":"636","trip_miles":"4.2579","pickup_census_tract":"17031062100","dropoff_census_tract":"17031081500","pickup_community_area":"6","dropoff_community_area":"8","fare":"10","tip":"5","additional_charges":"2.83","trip_total":"17.83","shared_trip_authorized":false,"trips_pooled":"1","pickup_centroid_latitude":"41.9426918444","pickup_centroid_longitude":"-87.6517705068","pickup_centroid_location":{"type":"Point","coordinates":[-87.6517705068,41.9426918444]},"dropoff_centroid_latitude":"41.8925077809","dropoff_centroid_longitude":"-87.6262149064","dropoff_centroid_location":{"type":"Point","coordinates":[-87.6262149064,41.8925077809]}}]`
+
+		body2, _ := ioutil.ReadAll(res2.Body)
+		var taxiTripsArray2 TAXI_TRIPS_DATA
+		// json.Unmarshal([]byte(jsonString2), &taxiTripsArray2)
+		json.Unmarshal(body2, &taxiTripsArray2)
 		for i := 0; i < len(taxiTripsArray2); i++ {
 
 			tripId := taxiTripsArray2[i].TRIP_ID
@@ -299,8 +397,50 @@ func main() {
 				dropoffLon = "0"
 			}
 
-			sql := "INSERT INTO taxi_trip (tripId, tripStartTime, tripEndTime, pickupLat, pickupLon, dropoffLat, dropoffLon, pickupZip, dropoffZip) values($1, $2, $3, $4, $5, $6, $7, $8, $9)"
-			_, err = tx.ExecContext(ctx,
+			pickUpLatFloat, _ := strconv.ParseFloat(pickupLat, 64)
+			pickupLonFloat, _ := strconv.ParseFloat(pickupLon, 64)
+			pickUpLocation := geocoder.Location{
+				Latitude:  pickUpLatFloat,
+				Longitude: pickupLonFloat,
+			}
+
+			fmt.Println(pickUpLatFloat)
+			fmt.Println(pickupLonFloat)
+			fmt.Println(pickUpLocation)
+
+			pickUpAddresses, _ := geocoder.GeocodingReverse(pickUpLocation)
+			pickUpAddress := pickUpAddresses[0]
+			pickUpZip := pickUpAddress.PostalCode
+
+			dropoffLatFloat, _ := strconv.ParseFloat(dropoffLat, 64)
+			dropoffLonFloat, _ := strconv.ParseFloat(dropoffLon, 64)
+			dropoffLocation := geocoder.Location{
+				Latitude:  dropoffLatFloat,
+				Longitude: dropoffLonFloat,
+			}
+			dropoffAddresses, _ := geocoder.GeocodingReverse(dropoffLocation)
+			dropoffAddress := dropoffAddresses[0]
+			dropoffZip := dropoffAddress.PostalCode
+
+			createdAt := time.Now()
+			updatedAt := time.Now()
+
+			sql := `INSERT INTO taxi_trip ("tripId", "tripStartTime", "tripEndTime", "pickUpLat", "pickUpLon", "dropOffLat", "dropOffLon", "pickUpZip", 
+			"dropOffZip", "createdAt", "updatedAt") values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`
+			// _, err = tx.ExecContext(ctx,
+			// 	sql,
+			// 	tripId,
+			// 	tripStartTime,
+			// 	tripEndTime,
+			// 	pickupLat,
+			// 	pickupLon,
+			// 	dropoffLat,
+			// 	dropoffLon,
+			// 	pickUpZip,
+			// 	dropoffZip,
+			// 	createdAt, updatedAt)
+
+			_, errCreate := db.Exec(
 				sql,
 				tripId,
 				tripStartTime,
@@ -309,19 +449,28 @@ func main() {
 				pickupLon,
 				dropoffLat,
 				dropoffLon,
-				0,
-				0)
+				pickUpZip,
+				dropoffZip,
+				createdAt, updatedAt)
 
-			if err != nil {
-				fmt.Printf("\n ERROR = ", err)
-				fmt.Printf("\n")
-				tx.Rollback()
-				return
+			// if err != nil {
+			// 	fmt.Printf("\n ERROR = ", err)
+			// 	fmt.Printf("\n")
+			// 	tx.Rollback()
+			// 	return
+			// }
+
+			if errCreate != nil {
+				panic(errCreate)
 			}
 		}
 
 		err = tx.Commit()
 		checkErr(err)
+	})
+
+	http.HandleFunc("/show", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Println("here i am")
 	})
 
 	http.ListenAndServe(":9090", nil)
