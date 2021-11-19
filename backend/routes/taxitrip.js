2c90fb7d33a68708aab3fd7e4e322d0417cd5946
Anjali-Veer <anjali.veer@hotmail.com>
2021-11-19 14:57:15 -0600

    Updated with section for raw query
    Updated with section for airport data + covid cases count

--- backend/routes/taxitrip.js
+++ backend/routes/taxitrip.js
@@ -1,6 +1,54 @@
 const router = require('express').Router();
 const axios = require('axios');
 const { TaxiTrip } = require('../models');
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('postgres://postgres:root@localhost:5432/chicago_business_intelligence');
+const {QueryTypes } = require('sequelize');
+
+router.get('/airportTaxi', async(req, res) => {
+
+    try {        
+        await sequelize.query(`select "pickUpZip","dropOffZip",count(*) as total_trips,covid_weekly.week_number,"week_start_date","week_end_date","cases_per_week" from taxi_trip trip, covid_weekly_data covid_weekly
+        where "dropOffZip" = "zip_code"::varchar
+        and "tripStartTime" between week_start_date and week_end_date
+        and "pickUpZip" in ('60666','60638')
+        group by "pickUpZip","dropOffZip",week_number,"week_start_date","week_end_date","cases_per_week"
+        order by "cases_per_week" desc`
+        , { type: QueryTypes.SELECT })        
+        .then(result => {
+            console.log(result);
+            var taxitripArray = result;
+            var dataArray = Array(taxitripArray.length);
+            for(var i = 0; i < taxitripArray.length; i++) {
+            dataArray[i] = {            
+                id: 0,                                    
+                pickUpZip: taxitripArray[i].pickUpZip,
+                dropOffZip: taxitripArray[i].dropOffZip,
+                totalTrips: taxitripArray[i].total_trips,
+                weekNumber: taxitripArray[i].week_number,
+                weekStartDate: taxitripArray[i].week_start_date,
+                weekEndDate: taxitripArray[i].week_end_date,
+                casesPerWeek: taxitripArray[i].cases_per_week
+             }
+            }
+
+            res.send({
+                message: 'success',
+                rows: dataArray,
+                status: '200'
+            });                
+        })
+        .catch(err =>{
+            console.log(err);
+        });        
+    } catch(error) {
+        console.log("ERROR = " , error);
+        res.status(500).json({            
+            message: 'Server Error.',
+            error: error
+        })
+    }    
+})
 
 router.get('/data', async (req, res) => {
     try {
