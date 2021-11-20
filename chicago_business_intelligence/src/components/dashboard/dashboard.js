ae2026ae3094c335faf752569cfb28d6d34631a2
Anjali-Veer <anjali.veer@hotmail.com>
2021-11-19 20:58:39 -0600

    Addition on dashboard

--- chicago_business_intelligence/src/components/dashboard/dashboard.js
+++ chicago_business_intelligence/src/components/dashboard/dashboard.js
@@ -21,6 +21,16 @@ const DashBoard = () => {
           info: 'We are giving various form of covid reports daily, weekly, and ' + 
           'monthly with google charts.',
           image: TaxiImage
+        },
+        {
+          header: 'Social Economy Information',
+          info: 'For industrial and neighborhood infrastructure investment,'+
+          'the city of Chicago is interested to invest in top 5 neighborhoods with'+
+          'highest unemployment rate and poverty rate and waive the fees for'+
+          'building permits in those neighborhoods in order to encourage businesses'+
+          'to develop and invest in those neighborhoods. Both, building permits and'+
+          'unemployment, datasets will be used in this report.',          
+          image: SocialEconomy
         }
     ];
 
